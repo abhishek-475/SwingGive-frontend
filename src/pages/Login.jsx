6f24cc6e9c9ffffc87ec8302988cf63df8663934
Abhishek Pavithran <abhishekpavi6@gmail.com>
2026-04-22 08:42:05 +0530

    login resolved

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Mail, Lock } from "lucide-react";
 import toast from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
